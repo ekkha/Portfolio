bc332c88e8a83cc00b13d65d38775e5de8e59bdc
Sulekha Ekka <120490065+ekkha@users.noreply.github.com>
2023-07-17 12:02:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed=new typed(".text",{
+    Strings:["Frontend Developer","YouTuber","Twiter","Instagram","FaceBook"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
